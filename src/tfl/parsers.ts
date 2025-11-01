52ff485199fe5487c46467709face53ac90d43f9
Simon McLean <simonmclean@Simons-MacBook-Pro-2.local>
2025-11-01 16:12:42 +0000

    refactoring

--- src/tfl/parsers.ts
+++ src/tfl/parsers.ts
@@ -3,39 +3,33 @@ import { Departure } from "../types";
 
 type DepartureWithDestinationId = Departure & { destinationId: string };
 
+function toStartOfMinute(date?: Date): Date | undefined {
+  date?.setSeconds(0, 0);
+  return date;
+}
+
 function parseDeparture(thing: unknown): DepartureWithDestinationId {
   const parser = parseObject(thing);
-  const destination = parser.prop("destinationName", parseString);
-  const destinationId = parser.prop("destinationNaptanId", parseString);
-  const scheduledDeparture = parser.optionalProp(
-    "scheduledTimeOfDeparture",
-    parseDate,
-  );
-  const estimatedDeparture = parser.optionalProp(
-    "estimatedTimeOfDeparture",
-    parseDate,
-  );
-  // Set seconds to zero (start of the minute) as we only want minute-level resolution.
-  // Otherwise a train might be considered delayed if it's running seconds later than scheduled.
-  scheduledDeparture?.setSeconds(0, 0);
-  estimatedDeparture?.setSeconds(0, 0);
-  const status = parser.prop("departureStatus", parseString);
-  const delayInformation = parser.optionalProp("cause", parseString);
+  const parseStr = (key: string) => parser.prop(key, parseString);
 
   return {
-    destination,
-    destinationId,
-    scheduledDeparture,
-    estimatedDeparture,
-    status,
-    delayInformation,
+    destination: parseStr("destinationName"),
+    destinationId: parseStr("destinationNaptanId"),
+    // Set seconds to zero (start of the minute) as we only want minute-level resolution.
+    // Otherwise a train might be considered delayed if it's running seconds later than scheduled.
+    scheduledDeparture: toStartOfMinute(
+      parser.optionalProp("scheduledTimeOfDeparture", parseDate),
+    ),
+    estimatedDeparture: toStartOfMinute(
+      parser.optionalProp("estimatedTimeOfDeparture", parseDate),
+    ),
+    status: parseStr("departureStatus"),
+    delayInformation: parser.optionalProp("cause", parseString),
   };
 }
 
 export function parseDepartures(
   response: unknown,
 ): DepartureWithDestinationId[] {
-  return parseArray(response).map((element) => {
-    return parseDeparture(element);
-  });
+  return parseArray(response).map((element) => parseDeparture(element));
 }
